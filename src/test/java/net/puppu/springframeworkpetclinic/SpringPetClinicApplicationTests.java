2e0dbe09186e9917f4a86536367f9f5dd220056d
Chunhawit <pupz28@gmail.com>
2022-01-24 01:18:03 +0100

    first commit

---
+++ src/test/java/net/puppu/springframeworkpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package net.puppu.springframeworkpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
