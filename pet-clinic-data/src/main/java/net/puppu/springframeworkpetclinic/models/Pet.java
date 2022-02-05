a30754ef5db53397d9386202f0df2ffd3906a855
Chunhawit <pupz28@gmail.com>
2022-02-06 00:41:26 +0100

    add base entity. Closed #10

--- pet-clinic-data/src/main/java/net/puppu/springframeworkpetclinic/models/Pet.java
+++ pet-clinic-data/src/main/java/net/puppu/springframeworkpetclinic/models/Pet.java
@@ -2,7 +2,7 @@ package net.puppu.springframeworkpetclinic.models;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
--- pet-clinic-data/src/main/java/net/puppu/springframeworkpetclinic/models/PetType.java
+++ pet-clinic-data/src/main/java/net/puppu/springframeworkpetclinic/models/PetType.java
@@ -1,6 +1,6 @@
 package net.puppu.springframeworkpetclinic.models;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
